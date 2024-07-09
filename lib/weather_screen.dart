f8ed15de01a5622db5ede89b0fb25911dd2543d4
arshak-codes <quantumexploreram@yahoo.com>
2024-07-09 13:26:22 +0530

    added main card to body

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -19,17 +19,66 @@ class WeatherScreen extends StatelessWidget {
               height: 24,
             ),
             onPressed: () {
-              // Implement your refresh logic here
+              print('refresh');
             },
           ),
         ],
       ),
-      body: const Center(
-        child: Text(
-          'Weather Content Here',
-          style: TextStyle(fontSize: 24, color: Colors.white),
+      body: const Padding(
+  padding: EdgeInsets.all(16.0),
+  child: Column(
+    children: [
+      SizedBox(
+        width: double.infinity,
+        child: Card(
+          shadowColor: Colors.blueAccent,
+          elevation: 8,
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Text(
+                  '26°C',
+                  style: TextStyle(fontSize: 32,fontWeight: FontWeight.bold), // Adjust font size as needed
+                ),
+                SizedBox(height: 8),
+                Text('🌧️',
+                style: TextStyle(fontSize: 45),),
+                SizedBox(height: 8),
+                Text('Rain',
+                style: TextStyle(fontSize: 32,fontFamily: 'CustomFont'),),
+                // Other content inside your main card
+              ],
+            ),
+          ),
         ),
       ),
+
+      SizedBox(height: 25),
+      Align(
+        alignment: Alignment.centerLeft,
+        child: Text('Weather Forecast',
+        style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold),),
+      ),
+      SizedBox(height: 8),
+      // Weather forecast card
+      Card(
+        child: Placeholder(
+          fallbackHeight: 150,
+        ),
+      ),
+      SizedBox(height: 25),
+      // Additional info card
+      Card(
+        child: Placeholder(
+          fallbackHeight: 150,
+        ),
+      ),
+    ],
+  ),
+),
+
+
     );
   }
 }
